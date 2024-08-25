11cdb1d2d8bc32706faf3ac4ecf607b9043046ae
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2024-08-26 01:11:57 +0800

    Возможность получать Кабинеты для каждого Пользователя. Отдельно можно получить Активный кабинет

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -43,5 +44,37 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Возвращает список кабинетов, к которым пользователь имеет отношение
+     * @return BelongsToMany
+     */
+    public function cabinets()
+    {
+        return $this->belongsToMany(Cabinet::class, 'user_cabinets', 'user_id', 'cabinet_id');
+    }
+
+    /**
+     * Возвращает сущность активного кабинета для пользователя
+     * @return Cabinet|null
+     */
+    public function getActiveCabinet()
+    {
+        $userId = $this->id;
+        $query = "
+            SELECT
+                cabinet_id
+            FROM
+                user_cabinets
+            WHERE
+                user_id = $userId
+                AND is_active = 1
+            LIMIT 1
+        ";
+
+        $result = DB::select($query);
+
+        return isset($result[0]->cabinet_id) ? Cabinet::getById($result[0]->cabinet_id) : null;
     }
 }
