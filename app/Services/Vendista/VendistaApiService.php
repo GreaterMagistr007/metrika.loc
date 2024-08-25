6f121160b6f9d04023443ab80298a4ce62876924
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2024-08-25 18:20:35 +0800

    Начало сервисам положено

---
+++ app/Services/Vendista/VendistaApiService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services\Vendista;
+
+/**
+ * документация по api:
+ * https://api.vendista.ru:99/swagger/index.html
+ */
+class VendistaApiService
+{
+    const VENDISTA_API_URL = 'https://api.vendista.ru:99';
+}
