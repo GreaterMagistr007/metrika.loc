6f224ae2582dd69de35d583d1b6b5806dd3ee54d
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2024-08-26 01:11:17 +0800

    Расширение сущности Кабинет

--- app/Models/Cabinet.php
+++ app/Models/Cabinet.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Cabinet extends Model
 {
     use HasFactory;
+
+
+    /**
+     * Возвращает сущность по её id
+     *
+     * @param $id
+     * @return self|null
+     */
+    public static function getById($id)
+    {
+        $id = (int)$id;
+        if ($id < 1) {
+            return null;
+        }
+
+        return self::where('id', $id)->first();
+    }
 }
