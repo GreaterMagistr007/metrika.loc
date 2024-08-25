940d09e856c0bf9cfbdd747fcc32bbf3f694827d
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2024-08-25 18:05:42 +0800

    нарезка шаблона темы

--- resources/views/layouts/cabinet.blade.php
+++ resources/views/layouts/cabinet.blade.php
@@ -11,15 +11,7 @@
 
 <main id="main" class="main">
 
-    <div class="pagetitle">
-        <h1>Dashboard</h1>
-        <nav>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard</li>
-            </ol>
-        </nav>
-    </div><!-- End Page Title -->
+    @include('layouts.blocks.pagetitle')
 
     @yield('content')
 
