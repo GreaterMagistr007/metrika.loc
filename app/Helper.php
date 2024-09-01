62b8700743652afb91794af52460aeac8a6890f2
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2024-09-01 22:26:24 +0800

    Валидация регистрации

--- app/Helper.php
+++ app/Helper.php
@@ -26,4 +26,54 @@ class Helper
 
         return $rnd;
     }
+
+    public static function onlyDigits($string)
+    {
+        $string = (string)$string;
+
+        return preg_replace('/[^0-9]/', '', $string);
+    }
+
+    /**
+     * Форматирует номер телефона
+     *
+     * @param string $phoneNumber
+     * @return string
+     */
+    public static function formatPhoneNumber($phoneNumber)
+    {
+        $phoneNumber = (string)$phoneNumber;
+
+        // Удаляем все нецифровые символы из номера
+        $phoneNumber = preg_replace('/[^0-9]/', '', $phoneNumber);
+
+        // По умолчанию номер невалидный
+        $result = false;
+
+        // Проверяем длину номера
+        if (strlen($phoneNumber) == 10) {
+            // Десятизначный номер, добавляем код страны
+            $result = "+7 (" .
+                substr($phoneNumber, 0, 3) .
+                ") " .
+                substr($phoneNumber, 3, 3) .
+                "-" .
+                substr($phoneNumber, 6, 2) .
+                "-" .
+                substr($phoneNumber, 8, 2);
+        }
+        if (strlen($phoneNumber) == 11) {
+            // Одиннадцатизначный номер с кодом страны "8", удаляем "8" и добавляем "+7"
+            $result = "+7 (" .
+                substr($phoneNumber, 1, 3) .
+                ") " .
+                substr($phoneNumber, 4, 3) .
+                "-" .
+                substr($phoneNumber, 7, 2) .
+                "-" .
+                substr($phoneNumber, 9, 2);
+        }
+
+        return $result;
+    }
 }
---
+++ app/Traits/ValidatorTrait.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Traits;
+
+use App\Helper;
+
+trait ValidatorTrait
+{
+    private function validateId($id)
+    {
+        $id = (int)$id;
+        if ($id < 1) {
+            return false;
+        }
+
+        return $id;
+    }
+
+    private function validateEmail($email)
+    {
+        $email = (string)$email;
+
+        if (strlen($email) < 7 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+
+        return $email;
+    }
+
+    private function validatePhone($phone)
+    {
+        $phoneDigitsLength = 11;
+
+        $p = Helper::onlyDigits(Helper::formatPhoneNumber($phone));
+
+        if (strlen($p) !== $phoneDigitsLength || $p !== $phone) {
+            return false;
+        }
+
+        return $phone;
+    }
+
+    private function validatePassword($password)
+    {
+        $password = (string)$password;
+        $errors = [];
+
+        $minLength = 6;
+        $maxLength = 30;
+
+        // Минимальная длина
+        if (strlen($password) < $minLength) {
+            $errors[] = "Пароль должен быть не менее 6 символов.";
+        }
+
+        // Максимальная длина
+        if (strlen($password) > $maxLength) {
+            $errors[] = "Пароль должен быть не более 30 символов.";
+        }
+
+        // Проверка наличия хотя бы одного большого символа
+        if (!preg_match('/[A-ZА-Я]/u', $password)) {
+            $errors[] = "Пароль должен содержать хотя бы одну заглавную букву.";
+        }
+
+        // Проверка наличия хотя бы одной цифры
+        if (!preg_match('/\d/', $password)) {
+            $errors[] = "Пароль должен содержать хотя бы одну цифру.";
+        }
+
+        // Возврат ошибки, если есть ошибки
+        return count($errors) ? $errors : true;
+    }
+}
